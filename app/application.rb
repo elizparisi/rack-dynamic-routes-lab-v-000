4bdfe81819e77d01d18c422a1f56279d333c4472
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-16 18:16:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       
       item = @@items.find{ |i| i.name == item_name}
       resp.write item.price
-    elsif req.path.match("/items")
+    elsif item
     else 
       resp.write "Route not found"
       resp.status = 404
